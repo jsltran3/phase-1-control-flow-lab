607eda98cc138b7e98b3e64c33a6293fc144ae64
Joseph <jsltran@gmail.com>
2021-12-15 20:53:09 -0800

    commit tree 60b002c5258b8703c6803da563e434111471ee12

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+let fare = 0; 
+
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance < 400) {
+    return 'This one is on me!'
+  } else if (distance > 2000 && distance < 2500) {
+    //newFare = fare + 30; 
+    return 'I will gladly take your thirty bucks.'
+  } else if (distance => 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(location) {
+  return location === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(responseTip){
   // Write your code here!
+  switch (responseTip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
+
 }
